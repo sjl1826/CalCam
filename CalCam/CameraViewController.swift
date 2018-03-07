02a7ee40a0402fafd824cd5a5e340d468d68c0fd
Samuel Lee <32909233+sjl1826@users.noreply.github.com>
2018-03-07 15:17:47 -0800

    Add files via upload
    moving camera view controller inside workspace

---
+++ CalCam/CameraViewController.swift
@@ -0,0 +1,87 @@
+//
+//  viewController2.swift
+//  nachenbois
+//
+//  Created by Samuel J. Lee on 3/6/18.
+//  Copyright © 2018 Samuel J. Lee. All rights reserved.
+//
+
+import UIKit
+import EventKit
+
+class CameraViewController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let imagePicker = UIImagePickerController()
+    
+    func cameraop() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            imagePicker.delegate = self
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else
+        {
+            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+    @IBAction func openCamera(_ sender: UIButton) {
+        cameraop()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // loads
+    }
+    
+    @IBOutlet weak var myImg: UIImageView!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            myImg.contentMode = .scaleToFill
+            myImg.image = pickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func reTake(_ sender: Any) {
+        cameraop()
+    }
+    
+    
+        @IBAction func addtoCalendar(_ sender: Any) {
+
+        let eventStore:EKEventStore = EKEventStore()
+        eventStore.requestAccess(to: .event) {(granted, error) in
+            
+            if(granted) && (error == nil){
+                print("granted\(granted)")
+                let event:EKEvent = EKEvent(eventStore: eventStore)
+                event.title = "Winter Showcase 2018"
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy/MM/dd HH:mm"
+                let startTime = formatter.date(from: "2018/03/12 18:00")
+                event.startDate = startTime
+                let endTime = formatter.date(from: "2018/03/12 20:00")
+                event.endDate = endTime
+                event.notes = "Theoria Foundations Choreography"
+                event.location = "UCLA Ackerman Grand Ballroom"
+                event.calendar = eventStore.defaultCalendarForNewEvents
+                do{
+                    try eventStore.save(event, span: .thisEvent)
+                    print("saved event")
+                }catch let error as NSError{
+                    print("error")
+                }
+
+            }
+            else{
+                print("error : \(error)")
+            }
+        }
+    }
+
+    }
+
